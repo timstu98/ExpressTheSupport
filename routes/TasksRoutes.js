7e7d683f763ded03f6e57b88f424969f4763582e
Ed <edwardmarkdixon.gmail.com>
2021-04-08 09:50:06 +0100

    Login protected all task routes Co-authored-by: JosephSimmons <JosephSimmons@users.noreply.github.com>

--- routes/TasksRoutes.js
+++ routes/TasksRoutes.js
@@ -6,10 +6,10 @@ const tasksController = require('../controllers/TasksController')
 
 router.post('/', authFunctions.authJWT, tasksController.createNewTask)
 
-router.get('/', tasksController.displayTaskList)
+router.get('/', authFunctions.authJWT, tasksController.displayTaskList)
 
-router.get('/:id', tasksController.displaySpecificTask)
+router.get('/:id', authFunctions.authJWT, tasksController.displaySpecificTask)
 
-router.put('/:id/update', tasksController.updateStatus) // may change this to PATCH to be more RESTful
+router.put('/:id/update', authFunctions.authJWT, tasksController.updateStatus) // may change this to PATCH to be more RESTful
 
 module.exports = router
